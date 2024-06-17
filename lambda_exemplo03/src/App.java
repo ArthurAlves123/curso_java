4d3d8f43a9c424da21cc061e2b028663cbf14595
Arthur Alves <arthuralvessilvafariabr@gmail.com>
2024-06-17 09:26:09 -0300

    Programação funcional e funções lambda

---
+++ lambda_exemplo03/src/App.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import entities.Product;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		double factor = 1.1;
+
+        // list.forEach(new PriceUpdate());
+
+        // Consumer<Product> cons = p -> {
+		// 	p.setPrice(p.getPrice() * factor);
+		// };
+
+        
+		
+		list.forEach(p -> p.setPrice(p.getPrice() * factor));
+	    list.forEach(System.out::println);
+
+
+	}
+}
